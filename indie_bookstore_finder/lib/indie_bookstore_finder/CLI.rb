e833516f96257b2621d48958a34525bf9941d75f
Elle Faraday <faraday.elle@gmail.com>
2017-12-10 03:32:01 +0000

    build on CLI methods logic

--- indie_bookstore_finder/lib/indie_bookstore_finder/CLI.rb
+++ indie_bookstore_finder/lib/indie_bookstore_finder/CLI.rb
@@ -4,31 +4,51 @@ class IndieBookstoreFinder::CLI
     puts "Welcome to Indie Bookstore Finder"
     puts ""
     puts "Find an independent bookseller near you!"
-    print_states
     state_page
   end
 
   def state_page
+    print_states
+    state_page_instructions
+  end
+
+  def state_page_instructions
     puts "Please select your state by entering the number from the list and pressing return."
     puts "To exit enter exit"
-    input = gets.strip.to_i
-    if input == "exit"
-      puts "Thank you for shopping independent! Happy Reading!"
-    elsif input ==
-      cities_page
+    state_number = gets.strip.to_i
+    if state_number == "exit"
+      exit_program
+    elsif #states includes state_number
+      cities_page(state_number)
     else
-      puts "Sorry, I don't recognize that input."
+      input_error_message
       state_page
     end
   end
 
-  def cities_page
-    puts "We found independent bookstores in these cities in State"
+  def cities_page(state_number)
+    puts "We found independent bookstores in these cities in #{state_number}"
     print_cities
+    cities_page_instructions
+  end
+
+  def cities_page_instructions
     puts "Please select a city by entering its number from the list and pressing return."
     puts "To return to the list of states enter 'states'"
     puts "To exit enter exit"
-    gets.strip.to_i
+    city = gets.strip.to_i
+    if city = "exit"
+      exit_program
+    elsif #cities includes city
+      store_list_page(city)
+    else
+      input_error_message
+      cities_page_instructions
+    end
+  end
+
+
+
     store_list_page
   end
 
@@ -56,5 +76,11 @@ class IndieBookstoreFinder::CLI
   def print_stores
   end
 
+  def exit_program
+    puts "Thank you for shopping independent! Happy Reading!"
+  end
+
+  def input_error_message
+  end
 
 end
