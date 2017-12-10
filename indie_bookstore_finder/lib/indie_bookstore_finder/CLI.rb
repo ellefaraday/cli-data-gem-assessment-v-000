301f9153948fb09a061ae76df6083900a83dc670
Elle Faraday <faraday.elle@gmail.com>
2017-12-10 03:48:44 +0000

    cont build CLI methods logic

--- indie_bookstore_finder/lib/indie_bookstore_finder/CLI.rb
+++ indie_bookstore_finder/lib/indie_bookstore_finder/CLI.rb
@@ -26,10 +26,10 @@ class IndieBookstoreFinder::CLI
     end
   end
 
-  def cities_page(state_number)
+  def cities_page(state)
     puts "We found independent bookstores in these cities in #{state_number}"
-    print_cities
-    cities_page_instructions
+    print_cities(state)
+    cities_page_instructions(state)
   end
 
   def cities_page_instructions
@@ -47,29 +47,56 @@ class IndieBookstoreFinder::CLI
     end
   end
 
-
-
-    store_list_page
-  end
-
-  def print_cities
+  def print_cities(state)
   end
 
   def print_states
   end
 
-  def store_list_page
-    puts "Here are the indie bookstores in City, State!"
-    print_stores
+  def store_list_page(city)
+    puts "Here are the indie bookstores in #{City}, #{State}!"
+    print_stores(city)
+  end
+
+  def store_list_page_instructions
     puts "See more information about a store by entering its number from the list and pressing return."
     puts "To return to the list of cities in State enter 'cities'"
     puts "To return to the list of states enter 'states'"
     puts "To exit enter exit"
-    gets.strip.to_i
-    store_page
+    store = gets.strip.to_i
+    if store == "exit"
+      exit_program
+    elsif store == "cities"
+      cities_page(state)
+    elsif store == "states"
+      state_page
+    elsif #stores includes store
+      store_page
+    else
+      input_error_message
+      store_list_page_instructions
+    end
   end
 
   def store_page
+    puts #store details
+    puts "To return to the list of stores in #{city} enter 'stores'"
+    puts "To return to the list of cities in #{state} enter 'cities'"
+    puts "To return to the list of states enter 'states'"
+    puts "To exit enter exit"
+    input = gets.strip.to_i
+    if input == "exit"
+      exit_program
+    elsif input == "cities"
+      cities_page(state)
+    elsif input == "states"
+      state_page
+    elsif input == "stores"
+      store_list_page(city)
+    else
+      input_error_message
+      store_page
+    end
   end
 
 
@@ -81,6 +108,7 @@ class IndieBookstoreFinder::CLI
   end
 
   def input_error_message
+    puts "Sorry I don't recognize that input."
   end
 
 end
