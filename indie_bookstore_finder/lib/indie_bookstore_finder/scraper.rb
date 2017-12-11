a4561eaaf7b3ee06f273bc2a64f7b8b15aa02d94
Elle Faraday <faraday.elle@gmail.com>
2017-12-11 02:02:28 +0000

    fixed scraper.get_cities

--- indie_bookstore_finder/lib/indie_bookstore_finder/scraper.rb
+++ indie_bookstore_finder/lib/indie_bookstore_finder/scraper.rb
@@ -8,7 +8,7 @@ class IndieBookstoreFinder::Scraper
     self.get_index_page.css("div.np-span6.centered-list a").each do |state|
       new_state = State.new
       new_state.name = state.css.text.gsub(" Bookstores", "")
-      new_state.url = state.css.attribute("href")
+      new_state.url = state.css.attribute("href").value
       States.all << new_state
     end
     States.all
@@ -19,11 +19,10 @@ class IndieBookstoreFinder::Scraper
   end
 
   def get_cities(state)
-    self.get_state_page(state).css("span.catItemsExtraFieldsValue").each do |city|
+    self.get_state_page(state).css("ul li:first:child span.catItemsExtraFieldsValue").each do |city|
       new_city = City.new
       new_city.name = city.css.text
       state.cities << new_city
-
     end
     state.cities
   end
---
+++ indie_bookstore_finder/lib/indie_bookstore_finder/scraper_test.rb
@@ -0,0 +1,5 @@
+require "nokogiri"
+require "pry"
+require 'open-uri'
+
+binding.pry
